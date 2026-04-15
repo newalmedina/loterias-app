d708da81d9b982196f765553d6b2b196654f7385
newal medina <ing.newal.medina@gmail.com>
2026-04-15 15:24:31 +0200

    validando jugadas

--- app/Http/Controllers/Api/ApiVentasController.php
+++ app/Http/Controllers/Api/ApiVentasController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Loterie;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use App\Models\Venta;
 use App\Models\VentaDetalle;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class ApiVentasController extends Controller
@@ -27,15 +29,82 @@ class ApiVentasController extends Controller
             'detalles.*.loteriaSecondId' => 'nullable|integer',
         ]);
 
+        $center = Auth::user()->center;
 
-        $error = false;
+        // $errorMessage = [
+        //     "Loteria Nac. cerrada",
+        //     "Num. 70 para  la lot, Nac supera el limite de 20$ habiendose jugado 10-20€",
+        // ];
+        $errorMessage = [];
+        //validar errores
+        foreach ($request->detalles as $detalle) {
+            $tipo = $detalle['tipo'];
+            //loteria cerrada o ya han salido resultados
+            if ($detalle["loteriaId"]) {
+                $loteria = Loterie::find($detalle["loteriaId"]);
 
-        $errorMessage = [
-            "Loteria Nac. cerrada",
-            "Num. 70 para  la lot, Nac supera el limite de 20$ habiendose jugado 10-20€",
-        ];
+                if (!$loteria->disponible) {
 
-        if (!$error) {
+                    $errorMessage[] = "$loteria->short_name ya ha cerrado ventas ($tipo)";
+                }
+            }
+            if ($detalle["loteriaSecondId"]) {
+                $segundaLoteria = Loterie::find($detalle["loteriaSecondId"]);
+
+                if (!$segundaLoteria->disponible) {
+
+                    $errorMessage[] = "$segundaLoteria->short_name ya ha cerrado ventas ($tipo)";
+                }
+            }
+
+            //validar que el monto jugado no supere las jugadas realizada monto limite
+            $montoJugadoDiaHoy = (float) (
+                Order::where("date", Carbon::now()->format("Y-m-d"))
+                ->myCenter()
+                ->join("order_details", "orders.id", "=", "order_details.order_id")
+                ->where("order_details.number", str_replace('-', '', $detalle['numero']))
+                ->where("order_details.loterie_id", $detalle['loteriaId'])
+                ->where("order_details.second_loterie_id", $detalle['loteriaSecondId'])
+                ->where("order_details.type", $detalle['tipo'])
+                ->sum("order_details.monto_jugada") ?? 0
+            );
+
+            switch ($detalle['tipo']) {
+                case 'Qui':
+                    $montoPermitido = $center->quiniela_monto_soportado;
+
+                    break;
+                case 'Pal':
+                    $montoPermitido = $center->pale_monto_soportado;
+                    break;
+                case 'SPal':
+                    $montoPermitido = $center->suppale_monto_soportado;
+                    break;
+                case 'Tri':
+                    $montoPermitido = $center->tripleta_monto_soportado;
+                    break;
+
+                default:
+                    break;
+            }
+
+            $montoNuevo = (float) ($detalle['monto'] ?? 0);
+
+            if (($montoJugadoDiaHoy + $montoNuevo) > $montoPermitido) {
+
+                $tipo = $detalle['tipo'];
+                $numero = str_replace('-', '', $detalle['numero']);
+                $diferenciaDisponible = max(0, $montoPermitido - $montoJugadoDiaHoy);
+                if ($detalle['tipo'] == "SPal") {
+                    $errorMessage[] = "($tipo)  {$numero} - {$loteria->short_name} - {$segundaLoteria->short_name}  ha superado el monto permitido diario ($montoPermitido$). Disponible: {$diferenciaDisponible}$.";
+                } else {
+                    $errorMessage[] = "($tipo)  {$numero} - {$loteria->short_name} ha superado el monto permitido diario ($montoPermitido$). Disponible: {$diferenciaDisponible}$.";
+                }
+            }
+        }
+
+
+        if (count($errorMessage) == 0) {
             $order = Order::create([
                 'date' => now(),
             ]);
